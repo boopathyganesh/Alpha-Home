1856ba3730b6391a47ea02df519e251688934df8
Pavin Kalyaan S S <pavinkalyaan263@gmail.com>
2024-06-16 02:21:57 +0530

    Expertise section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Hero from "@/components/hero";
 import Image from "next/image";
+import Expertise from "@/components/Expertise";
 
 export default function Home() {
   return (
     <section className="max-w-8xl mx-auto flex flex-col items-center justify-between p-10">
       <Hero />
+      <Expertise/>  
+
     </section>
   );
 }
---
+++ components/Expertise.tsx
@@ -0,0 +1,40 @@
+export default function Expertise() {
+  return (
+    <section className="max-w-4xl mt-10">
+      <div className="flex items-start justify-center">
+        <div className="w-1/2">
+          <h1 className="text-4xl leading-tight">
+            The best software teams ship early and often
+          </h1>
+        </div>
+        <div className="w-1/2">
+          <p className="text-base mb-5">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem,
+            quisquam dolore necessitatibus quia minus accusantium culpa
+            accusamus eos et expedita autem nemo. Dolorum tempora
+            exercitationem, amet quasi ab dolore assumenda?
+          </p>
+          <button className="px-5 py-2 rounded-lg font-medium text-white bg-blue-400 hover:bg-blue-500 ">
+            Watch Video
+          </button>
+        </div>
+      </div>
+      <div className="flex items-start justify-center mt-20">
+        <div className="w-1/2">bottomleft</div>
+        <div className="w-1/2">
+          <h2 className="font-semibold text-blue-400 mb-2">SOURCE & ATTRACT</h2>
+          <h1 className="text-4xl leading-tight mb-2">
+            All the info you need - in one place
+          </h1>
+          <p className="text-base my-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+            nesciunt mollitia facere nobis eveniet necessitatibus possimus
+            aperiam corporis vero quo! Totam sint illo at debitis reprehenderit
+            mollitia a dolorum possimus.
+          </p>
+          <button className="px-5 py-2 rounded-lg font-medium text-white bg-blue-400 hover:bg-blue-500">Learn More</button>
+        </div>
+      </div>
+    </section>
+  );
+}
