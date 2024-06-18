679d86178052628d3022ce99e9f34848d74d3f7d
Pavin Kalyaan S S <pavinkalyaan263@gmail.com>
2024-06-18 18:24:19 +0530

    Reasons Cards done

--- components/card.tsx
+++ components/card.tsx
@@ -1,4 +1,3 @@
-import globe from "@/public/images/SVG/globe.svg";
 import Image, { StaticImageData } from "next/image";
 interface CardProps {
   image: string | StaticImageData;
@@ -7,11 +6,11 @@ interface CardProps {
 }
 export default function Card({ image, title, description }: CardProps) {
   return (
-    <div className=" bg-white h-48 w-80 flex flex-col justify-evenly items-center text-center border rounded-xl border-blue-200 p-4 ">
+    <div className=" bg-white h-48 w-80 flex flex-col justify-evenly items-center text-center border rounded-xl border-blue-200 p-4 hover:bg-blue-100 smooth">
       <div className=" w-16 h-auto ">
         <Image src={image} alt="globe1" height={500} width={500} />
       </div>
-      <h2 className=" font-semibold text-indigo-300 ">{title}</h2>
+      <h2 className=" font-semibold text-indigo-400 ">{title}</h2>
       <p className=" font-medium ">
         {description}
       </p>
--- context/data.ts
+++ context/data.ts
@@ -1,21 +1,21 @@
 export const BackUp=[
     {
-        image: "/images/SVG/globe.svg", title: "World-Class Partner", description: "Keep things moving from anywhere with our top-rated mobile hiring app."
+        image: "/images/3D/worldclass.png", title: "World-Class Partner", description: "Keep things moving from anywhere with our top-rated mobile hiring app."
     },
     {
-        image: "/images/SVG/globe.svg", title: "World-Class Partner", description: "Keep things moving from anywhere with our top-rated mobile hiring app."
+        image: "/images/3d/global.png", title: "Fast,global support", description: "Keep things moving from anywhere with our top-rated mobile hiring app."
     },
     {
-        image: "/images/SVG/globe.svg", title: "World-Class Partner", description: "Keep things moving from anywhere with our top-rated mobile hiring app."
+        image: "/images/3D/sheild.png", title: "Trusted Security", description: "Keep things moving from anywhere with our top-rated mobile hiring app."
     },
     {
-        image: "/images/SVG/globe.svg", title: "World-Class Partner", description: "Keep things moving from anywhere with our top-rated mobile hiring app."
+        image: "/images/3D/recruit.png", title: "Anywhere recruiting", description: "Keep things moving from anywhere with our top-rated mobile hiring app."
     },
     {
-        image: "/images/SVG/globe.svg", title: "World-Class Partner", description: "Keep things moving from anywhere with our top-rated mobile hiring app."
+        image: "/images/3D/advice.png", title: "Expert advice", description: "Keep things moving from anywhere with our top-rated mobile hiring app."
     },
     {
-        image: "/images/SVG/globe.svg", title: "World-Class Partner", description: "Keep things moving from anywhere with our top-rated mobile hiring app."
+        image: "/images/3D/rocket.png", title: "Assisted Onboarding", description: "Keep things moving from anywhere with our top-rated mobile hiring app."
     },
     
 
